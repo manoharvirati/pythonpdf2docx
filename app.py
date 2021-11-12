20c494c12c87854c8341d1ad7b82d3d9b273112f
manoharvirati <40772700+manoharvirati@users.noreply.github.com>
2021-11-12 23:04:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from docx2pdf import convert
 from pdf2docx import parse
 from typing import Tuple
 import sys
-import tkinter import _tkinter
+from tkinter import _tkinter
 
 #import win32com.client
 #import pythoncom
